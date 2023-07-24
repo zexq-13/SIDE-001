0b3ab042a15fd9328f0a645ba4745e6ba9b64db2
zexq-13 <140268512+zexq-13@users.noreply.github.com>
2023-07-24 16:29:20 +0530

    Update QA.py

--- utils/QA.py
+++ utils/QA.py
@@ -3,7 +3,7 @@ import openai
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
 
-openai.api_key  = os.environ['OPENAI_API_KEY']
+openai.api_key  = os.environ['OPENAI_API_KEY'] #Enter OpenAI key here
 
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
